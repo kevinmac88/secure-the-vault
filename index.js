45e5d448791d09cbcc6fb060464e08dfce69a07f
Kevin MacDonald <iamkevinmac@gmail.com>
2025-09-05 14:39:36 -0700

    securing the vault assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const code1 = 7 + 3;
+
+const code2 = 120 / 2 - 20;
+
+const code3 = 79 % 40;
+
+const message = "The vault has been secured. The combination is:";
+
+const codeA = message + code1 + "-" + code2 + "-" + code3;
+
+const codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
